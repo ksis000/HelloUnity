a3ff206153aca388a154d226578b474f5c9167b4
Khoda <ksis000@aucklanduni.ac.nz>
2016-08-16 15:33:34 +1200

    added completed lab code
    adds a text to a prefab via api

--- HelloUnity/Assets/CubeScript.cs
+++ HelloUnity/Assets/CubeScript.cs
@@ -6,7 +6,7 @@ public class CubeScript : MonoBehaviour {
     public Vector3 spinSpeed = Vector3.zero;
     public Vector3 spinAxis = new Vector3(0, 1, 0);
     public float rotateSpeed = 1.0f;
-
+    
 	// Use this for initialization
 	void Start () {
         //setSize(2.0f);
--- HelloUnity/Assets/Sketch.cs
+++ HelloUnity/Assets/Sketch.cs
@@ -1,32 +1,77 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Pathfinding.Serialization.JsonFx; //make sure you include this using
 
-public class Sketch : MonoBehaviour {
-
+public class Sketch : MonoBehaviour
+{
+    public TextMesh nText = new TextMesh();
     public GameObject myPrefab;
+    public string _WebsiteURL = "http://ksis000lab2.azurewebsites.net/tables/product?zumo-api-version=2.0.0";
 
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
+        //Reguest.GET can be called passing in your ODATA url as a string in the form:
+        //http://{Your Site Name}.azurewebsites.net/tables/{Your Table Name}?zumo-api-version=2.0.0
+        //The response produce is a JSON string
+        string jsonResponse = Request.GET(_WebsiteURL);
 
+        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+
+        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
+        Product[] products = JsonReader.Deserialize<Product[]>(jsonResponse);
+
+        //----------------------
+        //YOU WILL NEED TO DECLARE SOME VARIABLES HERE SIMILAR TO THE CREATIVE CODING TUTORIAL
         int totalCubes = 8;
         int totalDistance = 5;
+        int i = 0;
+        
 
-        for(int i = 0; i < totalCubes; i++)
-        {
-            float perc = i / (float)totalCubes; 
+        //for (int i = 0; i < totalCubes; i++)
+        //{
+            /*float perc = i / (float)totalCubes;
 
             float x = perc * totalDistance;
             float y = 5.0f;
             float z = 0.0f;
 
-            var newCube = (GameObject) Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<CubeScript>().setSize(1.0f - perc);
+            newCube.GetComponent<CubeScript>().rotateSpeed = perc;*/
+        //}
+
+
+        //----------------------
+
+        //We can now loop through the array of objects and access each object individually
+        foreach (Product product in products)
+        {
+            //Example of how to use the object
+            Debug.Log("This products name is: " + product.ProductName);
+            //----------------------
+            //YOUR CODE TO INSTANTIATE NEW PREFABS GOES HERE
+            float perc = i / (float)totalCubes;
+
+            float x = perc * totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
             newCube.GetComponent<CubeScript>().setSize(1.0f - perc);
             newCube.GetComponent<CubeScript>().rotateSpeed = perc;
+            newCube.GetComponentInChildren<TextMesh>().text = product.ProductName;
+            i++;
+
+            //----------------------
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
